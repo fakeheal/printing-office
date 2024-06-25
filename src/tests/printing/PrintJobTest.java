1032e9746693c88584dbe4527ae20412b9822534
Ivanka Todorova <fakeheal@gmail.com>
2024-06-26 01:12:56 +0300

    quality(tests): add PrintJobTest

---
+++ src/tests/printing/PrintJobTest.java
@@ -0,0 +1,52 @@
+package printing;
+
+import exception.PrintingException;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class PrintJobTest {
+
+    @Test
+    void throwsExceptionIfPricePerCopyIsInvalid() {
+        assertThrows(
+                PrintingException.class,
+                () -> new PrintJob(
+                        new Publication("Title", "Issue", 10, new Paper(PaperSize.A3, PaperType.glossy)),
+                        1,
+                        false,
+                        BigDecimal.valueOf(-1)
+                )
+        );
+    }
+
+    @Test
+    void throwsExceptionIfCopiesIsNegative() {
+        assertThrows(
+                PrintingException.class,
+                () -> new PrintJob(
+                        new Publication("Title", "Issue", 10, new Paper(PaperSize.A3, PaperType.glossy)),
+                        -1,
+                        false,
+                        BigDecimal.valueOf(1)
+                )
+        );
+    }
+
+    @Test
+    void doesNotThrowExceptionIfDataIsValid() {
+        try {
+            new PrintJob(
+                    new Publication("Title", "Issue", 10, new Paper(PaperSize.A3, PaperType.glossy)),
+                    1,
+                    false,
+                    BigDecimal.valueOf(1)
+            );
+        } catch (PrintingException e) {
+            fail();
+        }
+    }
+}
