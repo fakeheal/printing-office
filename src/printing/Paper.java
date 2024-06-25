aeaa0cd8a8e4b82e97b84da0d6edc1e046d62c31
Ivanka Todorova <fakeheal@gmail.com>
2024-06-26 00:55:28 +0300

    quality(refactor): Paper to be a record, not a class

--- src/printing/Paper.java
+++ src/printing/Paper.java
@@ -1,19 +1,4 @@
 package printing;
 
-public class Paper {
-    private final PaperSize size;
-    private final PaperType paperType;
-
-    public Paper(PaperSize size, PaperType paperType) {
-        this.size = size;
-        this.paperType = paperType;
-    }
-
-    public PaperSize getSize() {
-        return size;
-    }
-
-    public PaperType getPaperType() {
-        return paperType;
-    }
+public record Paper(PaperSize size, PaperType paperType) {
 }
